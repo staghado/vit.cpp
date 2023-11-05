0bc5569f73127b1f20e59fafa001f3175bd6665b
staghado <taghadouinisaid@gmail.com>
2023-11-05 17:55:04 +0100

    include ggml and ggml-alloc

--- vit.cpp
+++ vit.cpp
@@ -1,5 +1,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h" // load image from file using STB open source implementation
+#include "./ggml/ggml.h"
+#include "./ggml/ggml-alloc.h"
 
 #include <cassert>   // provides assertion
 #include <cmath>     // sin, cos, M_PI
@@ -8,14 +10,14 @@
 #include <cstring>   // string maanipulation like strcpy(), strlen(), and strcmp()
 #include <fstream>   // read from and write to files
 #include <map>       // key-value container
-#include <string>    // more flexible strings than C-style 
+#include <string>    // more flexible strings than C-style
 #include <vector>    // standard 'vector' dynamic container
 #include <thread>    // manage threads
 #include <cinttypes> // format macros for integer types across-platforms like PRId64, PRIu32, etc
 
-
 // default ViT hparams (vit_base_patch8_224.augreg2_in21k_ft_in1k from timm)
-struct vit_hparams {
+struct vit_hparams
+{
     int32_t hidden_size = 768;
     int32_t intermediate_size = 3072;
     int32_t num_hidden_layers = 12;
@@ -25,8 +27,7 @@ struct vit_hparams {
     int32_t ftype = 1;
     float eps = 1e-6f;
 
-    int32_t n_img_size()     const { return 224; }
-    int32_t n_patch_size()   const { return 8; }
-    int32_t n_img_embd()     const { return n_img_size() / n_patch_size(); }
-
+    int32_t n_img_size() const { return 224; }
+    int32_t n_patch_size() const { return 8; }
+    int32_t n_img_embd() const { return n_img_size() / n_patch_size(); }
 };
